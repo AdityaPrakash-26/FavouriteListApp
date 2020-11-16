50584a3c6ea23725b751a3ce19bf017dc9367900
Aditya Prakash <prakashadityaap@gmail.com>
2020-11-16 16:07:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/favouritelistapp/CategoryViewHolder.java
@@ -0,0 +1,11 @@
+package com.example.favouritelistapp;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CategoryViewHolder extends RecyclerView.ViewHolder {
+    public CategoryViewHolder(View view){
+        super(view);
+    }
+}
